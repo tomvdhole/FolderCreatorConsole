5b73e39cf84763695d03832811548ff77ee3fa83
Tom VdH <tomvdhole@gmail.com>
2024-10-21 15:06:17 +0200

    up

--- FolderCreation/Features/ShowDisplayStatus/ResultExtensionsForDisplayStatus.cs
+++ FolderCreation/Features/ShowDisplayStatus/ResultExtensionsForDisplayStatus.cs
@@ -23,13 +23,13 @@ public static class ResultExtensionsForDisplayingStatus
                     var resultFolder = processedRow.GetResultFolder();
                     if (resultFolder is null || !resultFolder.IsSuccess)
                     {
-                        writer($"{processedRow.RowNumber} - NOT-OK - {(resultFolder is null ? string.Empty : string.Join(", ", resultFolder.Errors))}");
+                        writer($"{processedRow.RowNumber} - NOT-OK - {(resultFolder is null ? string.Empty : string.Join(", ", resultFolder.Errors).MakeErrorsUserFriendlyer())}");
                     }
                     else
                     {
                         var resultCreatedFolder = folderCreator(resultFolder.Value!);
                         if (!resultCreatedFolder.IsSuccess)
-                            writer($"{processedRow.RowNumber} - NOT-OK - {(resultCreatedFolder is null ? string.Empty : string.Join(", ", resultCreatedFolder.Errors))}");
+                            writer($"{processedRow.RowNumber} - NOT-OK - {(resultCreatedFolder is null ? string.Empty : string.Join(", ", resultCreatedFolder.Errors).MakeErrorsUserFriendlyer())}");
                         else
                             writer($"{processedRow.RowNumber} - OK - {resultFolder.Value!.Name}");
                     }
